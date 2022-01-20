ef80db5a7383d0df674cbd00ab2f91ee0ed1f8ee
DESKTOP-P1244TV\linyuxuan <candy0819coco@gmail.com>
2022-01-20 11:53:35 +0800

    SCSS commit

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -14,18 +14,19 @@ const Navbar = () => {
         <div className="mainLogoArea">
           <div className="mainLogo"></div>
         </div>
-        <div className="containerRight"></div>
-        <div className="containerText">
-          <div className="navAbout">About</div>
-          <div className="navLineUp">Line up</div>
-          <div className="navTicket">Ticket</div>
-          <div className="navMap">Map</div>
-          <div className="navShop">Shop</div>
-        </div>
-        <div className="iconArea">
-          <div className="icon toggleBtn"></div>
-          <div className="icon cartIcon"></div>
-          <div className="icon userIcon"></div>
+        <div className="containerRight">
+          <div className="containerText">
+            <div className="navAbout">About</div>
+            <div className="navLineUp">Line up</div>
+            <div className="navTicket">Ticket</div>
+            <div className="navMap">Map</div>
+            <div className="navShop">Shop</div>
+          </div>
+          <div className="iconArea">
+            <div className="icon toggleBtn"></div>
+            <div className="icon cartIcon"></div>
+            <div className="icon userIcon"></div>
+          </div>
         </div>
       </div>
     </div>
