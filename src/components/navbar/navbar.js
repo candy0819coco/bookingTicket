031e5d4c0dd15da1bf5c83f46e1c61779dd42d74
DESKTOP-P1244TV\linyuxuan <candy0819coco@gmail.com>
2022-01-28 17:25:37 +0800

    教大家

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,53 +6,54 @@ import React, {
   useContext,
 } from "react";
 import "./Navbar.scss";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import context, { Provider } from "./../context";
 
-const Navbar = (props) => {
-  const { isDarkMode, setIsDarkMode } = props;
 
-  const handleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
+const Navbar = () => {
+  const contextValue = useContext(context);
+  const { pathName, setPathName, isDarkMode, setIsDarkMode } = contextValue;
   return (
-    <div className="navbar_container">
-      <div className={`navbar_inner ${isDarkMode ? "navbar_inner_dark" : ""}`}>
-        <div className="main_logo_area">
-          <div
-            className={`main_logo ${isDarkMode ? "main_logo_dark" : ""}`}
-          ></div>
-        </div>
-        <div className="container_right">
-          <div
-            className={`menu_item_area ${
-              isDarkMode ? "menu_item_area_dark" : ""
-            }`}
-          >
-            <div className="nav_item">About</div>
-            <div className="nav_item">Line up</div>
-            <div className="nav_item">Ticket</div>
-            <div className="nav_item">Map</div>
-            <div className="nav_item">Shop</div>
+    <Provider value={contextValue}>
+      <div className="navbar_container">
+        <div className={`navbar_inner ${isDarkMode ? "navbar_inner_dark" : ""}`}>
+          <div className="main_logo_area">
+            <div
+              className={`main_logo ${isDarkMode ? "main_logo_dark" : ""}`}
+            ></div>
           </div>
-          <div className="func_area">
-            <div className={`sign_in ${isDarkMode ? "sign_in_dark" : ""}`}>
-              Sign in
+          <div className="container_right">
+            <div
+              className={`menu_item_area ${
+                isDarkMode ? "menu_item_area_dark" : ""
+              }`}
+            >
+              <div className="nav_item">About</div>
+              <div className="nav_item">Line up</div>
+              <div className="nav_item">Ticket</div>
+              <div className="nav_item">Map</div>
+              <div className="nav_item">Shop</div>
             </div>
-            <div className="switch_toggle" onClick={handleDarkMode}>
+            <div className="func_area">
+              <div className={`sign_in ${isDarkMode ? "sign_in_dark" : ""}`}>
+                Sign in
+              </div>
+              <div className="switch_toggle"  onClick={() => setIsDarkMode([!isDarkMode])}>
+                <div
+                  className={`toggle_btn ${isDarkMode ? "toggle_btn_dark" : ""}`}
+                ></div>
+              </div>
               <div
-                className={`toggle_btn ${isDarkMode ? "toggle_btn_dark" : ""}`}
+                className={`icon icon_cart ${isDarkMode ? "icon_cart_dark" : ""}`}
+              ></div>
+              <div
+                className={`icon icon_user ${isDarkMode ? "icon_user_dark" : ""}`}
               ></div>
             </div>
-            <div
-              className={`icon icon_cart ${isDarkMode ? "icon_cart_dark" : ""}`}
-            ></div>
-            <div
-              className={`icon icon_user ${isDarkMode ? "icon_user_dark" : ""}`}
-            ></div>
           </div>
         </div>
       </div>
-    </div>
+    </Provider>
   );
 };
-
 export default Navbar;
